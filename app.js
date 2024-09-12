f39982c7ce4b17ad79906cae924b15db2b1a7a15
Antony <antony.caiquec@gmail.com>
2024-09-12 20:00:06 +0300

    configurações de dependencia mongodb (db em nuvem)

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 import express from 'express';
+import path from 'path';
+import { MongoClient, ObjectId } from 'mongodb';
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
+import cors from 'cors';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'client/src')));
+
+app.use('/assets', express.static(path.join(__dirname, 'client/assets')));
+let db;
+
+async function connect() {
+    if (db) return db;
+    
+    try {
+        const conn = await MongoClient.connect("mongodb+srv://grupo35:20141877@cluster0.hzthe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        if (!conn) throw new Error("Erro na conexão");
+        db = conn.db("unifor");
+        return db;
+    } catch (error) {
+        console.error("Erro na conexão com o banco de dados:", error);
+        throw error;
+    }
+}
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Peneira!`);
+    const name = process.env.NAME || 'World';
+    res.send(`Peneira!`);
 });
 
-const port = parseInt(process.env.PORT) || 3000;
+const port = parseInt(process.env.PORT) || 3001;
 app.listen(port, () => {
-  console.log(`Rodando na porta ${port}`);
+    console.log(`Rodando na porta ${port}`);
 });
